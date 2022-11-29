0612676d1f91ff3433427df9d676204a2ed7b73b
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2022-11-29 20:29:24 +0100

    Locked the pages just in case

--- js/daychecker.js
+++ js/daychecker.js
@@ -1,11 +1,19 @@
 const today = new Date().getDate();
+let content = '';
 
 try {
-    if (today < day) {
-        content = `<h3>Ezt még nem kéne látnod! ${today}/${day}</h3>`;
-    }
+    if (debug) { }
 } catch (error) {
-    content = '<h3>Ádám elfelejtette megadni, hogy ez az oldal melyik naphoz tartozik.</h3>';
-}
+    try {
+        if (today < day) {
+            content = `<h3>Ezt még nem kéne látnod! ${today}/${day}</h3>`;
+        }
+        else if (today > 24) {
+            content = '<h3>Még nincs December</h3>';
+        }
+    } catch (error) {
+        content = '<h3>Ádám elfelejtette megadni, hogy ez az oldal melyik naphoz tartozik.</h3>';
+    }
 
-document.body.innerHTML = content;
+    document.body.innerHTML = content;
+}

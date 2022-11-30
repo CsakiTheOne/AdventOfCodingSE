e03a91ddb4e98df3e3372a190111e4cd0955e045
Csáki (Csáktornyai Ádám József) <jockahun@gmail.com>
2022-12-01 00:05:58 +0100

    Simplified daychecker

--- js/daychecker.js
+++ js/daychecker.js
@@ -3,11 +3,7 @@ const today = new Date().getDate();
 try {
     if (debug) { }
 } catch (error) {
-    try {
-        if (today < day) {
-            document.body.innerHTML = `<h3>Ezt még nem kéne látnod! ${today}/${day}</h3>`;
-        }
-    } catch (error) {
-        document.body.innerHTML = '<h3>Ádám elfelejtette megadni, hogy ez az oldal melyik naphoz tartozik.</h3>';
+    if (today < day) {
+        document.body.innerHTML = `<h3>Ezt még nem kéne látnod! ${today}/${day}</h3>`;
     }
 }
